8ec6f897a99fe07427044ed42f665c8ed54e232c
lizhiwei <0049003294@znv.com>
2020-09-18 10:11:42 +0800

    '截图'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <znv-video v-if='item.src' :src="item.src" :type="item.type" :vId="item.vId" :playerType='item.playerType'></znv-video>
+    <znv-video v-if='item.src' :src="item.src" :type="item.type" :vId="item.vId" :playerType='item.playerType' :ref="item.vId"></znv-video>
   </div>
 </template>
 
@@ -13,19 +13,22 @@ export default {
   data () {
     return {
       item: {
-        src: '',
-        type: '',
+        src: 'http://img.ksbbs.com/asset/Mon_1704/15868902d399b87.flv',
+        type: 'flv',
         vId: 'znv' + +new Date(),
         playerType: 'videoJs'
       }
     }
   },
   mounted() {
-    let queryObj = st.url2json(location.href)
-    queryObj.src && (this.item.src = queryObj.src)
-    queryObj.type && (this.item.type = queryObj.type)
-    queryObj.vId && (this.item.vId = queryObj.vId)
-    queryObj.playerType && t(his.item.playerType = queryObj.playerType)
+    // let queryObj = st.url2json(location.href)
+    // queryObj.src && (this.item.src = queryObj.src)
+    // queryObj.type && (this.item.type = queryObj.type)
+    // queryObj.vId && (this.item.vId = queryObj.vId)
+    // queryObj.playerType && t(his.item.playerType = queryObj.playerType)
+    setTimeout(() => {
+      console.log(this.$refs[this.item.vId].getCurrentFrame())
+    }, 5000);
   }
 }
 </script>
--- src/components/znvVideo/ZnvVideo.vue
+++ src/components/znvVideo/ZnvVideo.vue
@@ -1,40 +1,50 @@
 <template>
-    <div class="znv-video" @mouseenter="soltShow = true" @mouseleave="soltShow = false">
-      <div v-if="type === 'hls'" class="hls-container">
-        <video v-if="playerType === 'videoJs'" :id="vId" style="width: 100%; height:100%;" class="video-js vjs-default-skin vjs-big-play-centered video"></video>
-        <video v-else muted :id="vId" class="video" autoplay controls></video>
-      </div>
-      <video v-if="type === 'rtsp'" :id="vId" controls autoplay muted class="video">
-        <source :src="src" type="application/x-rtsp" />
-      </video>
-      <div v-if="type === 'rtmp'" class="hls-container">
-        <video v-if="playerType === 'videoJs'" :id="vId" style="width: 100%; height:100%;" class="video-js vjs-default-skin vjs-big-play-centered video">
-          <source :src="src" type="rtmp/flv">
-        </video>
-        <video v-else :id="vId" class="video" autoplay controls muted playsInline webkit-playsinline></video>
-      </div>
-      <video v-if="type === 'mp4'" :src="src" :id="vId" controls autoplay muted loop class="video"></video>
-      <video v-if="type === 'flv'" :src="src" :id="vId" controls autoplay muted loop class="video"></video>
-      <transition name="fade">
-        <div class="opt-box" v-show="$scopedSlots.$hasNormal && soltShow">
-          <slot></slot>
-        </div>
-      </transition>
+  <div class="znv-video" @mouseenter="soltShow = true" @mouseleave="soltShow = false">
+    <div v-if="type === 'hls'" class="hls-container">
+      <video
+        v-if="playerType === 'videoJs'"
+        :id="vId"
+        style="width: 100%; height:100%;"
+        class="video-js vjs-default-skin vjs-big-play-centered video"
+      ></video>
+      <video v-else muted :id="vId" class="video" autoplay controls></video>
     </div>
+    <video v-if="type === 'rtsp'" :id="vId" controls autoplay muted class="video">
+      <source :src="src" type="application/x-rtsp" />
+    </video>
+    <div v-if="type === 'rtmp'" class="hls-container">
+      <video
+        v-if="playerType === 'videoJs'"
+        :id="vId"
+        style="width: 100%; height:100%;"
+        class="video-js vjs-default-skin vjs-big-play-centered video"
+      >
+        <source :src="src" type="rtmp/flv" />
+      </video>
+      <video v-else :id="vId" class="video" autoplay controls muted playsinline webkit-playsinline></video>
+    </div>
+    <video v-if="type === 'mp4'" :src="src" :id="vId" controls autoplay muted loop class="video"></video>
+    <video v-if="type === 'flv'" :src="src" :id="vId" controls autoplay muted loop class="video"></video>
+    <transition name="fade">
+      <div class="opt-box" v-show="$scopedSlots.$hasNormal && soltShow">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
 /**
-* 流媒体播放器
-* @module 组件存放位置
-* @desc 同时支持rtsp、rtmp、flv和hls四种流媒体，hls、rtmp支持VideoJsPlayer和EZUIPlayer两种解码库
-* @author 李志伟0049003294
-* @date 2019年11月23日10:22:43
-* @param {String} [src]    - 流媒体链接 必传
-* @param {String} [vId] - 播放器ID 必传
-* @param {String} [type]    - 流媒体类型 必传
-* @param {String} [playerType] - 针对HLS、RTMP类型的流媒体指定解码库 非必传 EZUIPlayer videoJs
-*/
+ * 流媒体播放器
+ * @module 组件存放位置
+ * @desc 同时支持rtsp、rtmp、flv和hls四种流媒体，hls、rtmp支持VideoJsPlayer和EZUIPlayer两种解码库
+ * @author 李志伟0049003294
+ * @date 2019年11月23日10:22:43
+ * @param {String} [src]    - 流媒体链接 必传
+ * @param {String} [vId] - 播放器ID 必传
+ * @param {String} [type]    - 流媒体类型 必传
+ * @param {String} [playerType] - 针对HLS、RTMP类型的流媒体指定解码库 非必传 EZUIPlayer videoJs
+ */
 import videojs from 'video.js'
 import 'videojs-flash'
 import 'video.js/dist/video-js.css'
@@ -50,7 +60,7 @@ if (!window.EZUIPlayer) {
 // videojs.options.flash.swf = 'video-js.swf'
 videojs.addLanguage('zh-CN', {
   ...language.language,
-  'Picture-in-Picture': '画中画'
+  'Picture-in-Picture': '画中画',
 })
 
 export default {
@@ -58,32 +68,32 @@ export default {
   data() {
     return {
       soltShow: false,
-      isSmallDom: false
+      isSmallDom: false,
     }
   },
   props: {
     src: {
       type: String,
       required: true,
-      default: ''
+      default: '',
     },
     vId: {
       type: String,
       required: true,
-      default: ''
+      default: '',
     },
     type: {
       type: String,
       required: true,
-      default: ''
+      default: '',
     },
     playerType: {
       type: String,
       required: false,
-      default: 'videoJs'
-    }
+      default: 'videoJs',
+    },
   },
-  mounted () {
+  mounted() {
     if (this.type === 'hls') {
       this.initHls()
     } else if (this.type === 'rtsp') {
@@ -95,14 +105,14 @@ export default {
     }
   },
   watch: {
-    src () {
+    src() {
       this.initZnvVideo()
       if (this.type === 'hls') {
         if (this.playerType === 'videoJs') {
           this.player.src({
             src: this.src,
             type: 'application/x-mpegURL',
-            withCredentials: false
+            withCredentials: false,
           })
         } else {
           this.$nextTick(() => {
@@ -119,7 +129,7 @@ export default {
         if (this.playerType === 'videoJs') {
           this.player.src({
             src: this.src,
-            type: 'rtmp/flv'
+            type: 'rtmp/flv',
           })
         } else {
           this.$nextTick(() => {
@@ -133,36 +143,38 @@ export default {
       } else if (this.type === 'flv') {
         this.initFlv()
       }
-    }
+    },
   },
   methods: {
     // 切换视频流时调用，初始化播放器
     initZnvVideo() {
       if (this.player) {
         switch (this.player.znvVideoSrcType) {
-          case 'hls' :
+          case 'hls':
             if (this.player.znvVideoPlayerType === 'EZUIPlayer') {
               this.player.stop()
               $('#' + this.vId).attr('src', '')
             }
             break
-          case 'rtsp' :
+          case 'rtsp':
             this.destroyRtsp()
             break
-          case 'rtmp' :
+          case 'rtmp':
             if (this.player.znvVideoPlayerType === 'EZUIPlayer') {
               this.player.stop()
-              $('#' + this.vId).replaceWith(`<video id="${this.vId}" class="video" autoplay controls muted></video>`)
+              $('#' + this.vId).replaceWith(
+                `<video id="${this.vId}" class="video" autoplay controls muted></video>`
+              )
             }
             break
-          case 'flv' :
+          case 'flv':
             this.destroyFlv()
             break
         }
       }
     },
     // 初始化HLS视频流
-    initHls () {
+    initHls() {
       if (!this.src) {
         return
       }
@@ -180,13 +192,13 @@ export default {
             currentTimeDisplay: false, // 当前播放时间
             timeDivider: false, // 时间分割线
             durationDisplay: false, // 总时间
-            remainingTimeDisplay: false // 剩余时间，
-          }
+            remainingTimeDisplay: false, // 剩余时间，
+          },
         })
         this.player.src({
           src: this.src,
           type: 'application/x-mpegURL',
-          withCredentials: false
+          withCredentials: false,
         })
         this.player.znvVideoSrcType = 'hls'
         this.player.znvVideoPlayerType = 'videoJs'
@@ -201,7 +213,7 @@ export default {
       }
     },
     // 初始化RTSP视频流
-    initRtsp () {
+    initRtsp() {
       this.$nextTick(() => {
         if (window.Streamedian && this.src) {
           let errHandler = function (err) {
@@ -211,19 +223,16 @@ export default {
             socket: sysConfig.rtspWebSocketUrl,
             redirectNativeMediaErrors: true,
             bufferDuration: 30,
-            errorHandler: errHandler
+            errorHandler: errHandler,
           }
-          this.player = window.Streamedian.player(
-            this.vId,
-            playerOptions
-          )
+          this.player = window.Streamedian.player(this.vId, playerOptions)
           this.player.znvVideoSrcType = 'rtsp'
           this.player.znvVideoPlayerType = 'streamedian'
         }
       })
     },
     // 初始化RTMP视频流
-    initRtmp () {
+    initRtmp() {
       if (!this.src) {
         return
       }
@@ -241,12 +250,12 @@ export default {
               currentTimeDisplay: false, // 当前播放时间
               timeDivider: false, // 时间分割线
               durationDisplay: false, // 总时间
-              remainingTimeDisplay: false // 剩余时间，
-            }
+              remainingTimeDisplay: false, // 剩余时间，
+            },
           })
           this.player.src({
             src: this.src,
-            type: 'rtmp/flv'
+            type: 'rtmp/flv',
           })
           this.player.znvVideoSrcType = 'rtmp'
           this.player.znvVideoPlayerType = 'videoJs'
@@ -271,32 +280,35 @@ export default {
       this.$nextTick(() => {
         if (flvjs.isSupported() && this.src) {
           let videoElement = document.getElementById(this.vId)
-          this.player = flvjs.createPlayer({
-            type: 'flv',
-            url: this.src
-          }, {
-            isLive: true,                
-            enableStashBuffer: false,
-            autoCleanupMaxBackwardDuration: 60,
-            autoCleanupMinBackwardDuration: 30,
-            statisticsInfoReportInterval: 2000,
-            stashInitialSize: 128 * 1024,
-            // 如果是Android浏览器，建议enableDurationMonitor设置为false
-            enableDurationMonitor: enableDurationMonitor,    // true表示监测当前直播流延时，当发现延时过大时，主动追赶
-            enableVideoFrozenMonitor: true, // 监测视频解码是否停滞（画面卡停），当因为某些原因导致无法解码时，将上报VIDEO_FROZEN事件，收到后建议重拉流
-            videoStateMonitorInterval: 1000, // 多长时间（毫秒）检查一次视频状态（延时、停滞）
-            // 针对手机浏览器上对MSE以及网络连接更加容易不稳定，建议将maxDurationGap设置高一点，比如2.5、3、3.5，否则可能会频繁追赶延时导致画面卡顿
-            maxDurationGap: 1.5,         // 当前播放位置与缓冲区末尾的距离（秒）如果超过这个值，就触发一次追赶，不易过短
-            decreaseDurationStep: 0.4,   // 每次追赶至缓冲区末尾之前的多少秒
-            frozenTimesThreshold: 5,        // 解码停滞次数达到此阈值，上报VIDEO_FROZEN事件。注意如果设置过小的阈值，当推流端关闭摄像头后可能会频繁触发VIDEO_FROZEN事件
-            // webrtc合流未能给cdn推送正确的视频分辨率信息，导致从MetaData/AVCDecoderConfigurationRecord中无法拿到正确的视频分辨率
-            // 在内核低于Chromium 70的浏览器（如360浏览器、搜狗浏览器、PC微信内嵌浏览器等）中
-            // 如果传递不对的视频分辨率，将会导致画面放大显示异常，因此，强行设置一个最大的视频宽高信息，来避免此问题
-            // 如果是Safari浏览器，enableConstVideoViewSize建议设置为false
-            enableConstVideoViewSize: true, 
-            // constVideoViewWidth: 1920,
-            // constVideoViewHeight: 1080,
-          })
+          this.player = flvjs.createPlayer(
+            {
+              type: 'flv',
+              url: this.src,
+            },
+            {
+              isLive: true,
+              enableStashBuffer: false,
+              autoCleanupMaxBackwardDuration: 60,
+              autoCleanupMinBackwardDuration: 30,
+              statisticsInfoReportInterval: 2000,
+              stashInitialSize: 128 * 1024,
+              // 如果是Android浏览器，建议enableDurationMonitor设置为false
+              enableDurationMonitor: enableDurationMonitor, // true表示监测当前直播流延时，当发现延时过大时，主动追赶
+              enableVideoFrozenMonitor: true, // 监测视频解码是否停滞（画面卡停），当因为某些原因导致无法解码时，将上报VIDEO_FROZEN事件，收到后建议重拉流
+              videoStateMonitorInterval: 1000, // 多长时间（毫秒）检查一次视频状态（延时、停滞）
+              // 针对手机浏览器上对MSE以及网络连接更加容易不稳定，建议将maxDurationGap设置高一点，比如2.5、3、3.5，否则可能会频繁追赶延时导致画面卡顿
+              maxDurationGap: 1.5, // 当前播放位置与缓冲区末尾的距离（秒）如果超过这个值，就触发一次追赶，不易过短
+              decreaseDurationStep: 0.4, // 每次追赶至缓冲区末尾之前的多少秒
+              frozenTimesThreshold: 5, // 解码停滞次数达到此阈值，上报VIDEO_FROZEN事件。注意如果设置过小的阈值，当推流端关闭摄像头后可能会频繁触发VIDEO_FROZEN事件
+              // webrtc合流未能给cdn推送正确的视频分辨率信息，导致从MetaData/AVCDecoderConfigurationRecord中无法拿到正确的视频分辨率
+              // 在内核低于Chromium 70的浏览器（如360浏览器、搜狗浏览器、PC微信内嵌浏览器等）中
+              // 如果传递不对的视频分辨率，将会导致画面放大显示异常，因此，强行设置一个最大的视频宽高信息，来避免此问题
+              // 如果是Safari浏览器，enableConstVideoViewSize建议设置为false
+              enableConstVideoViewSize: true,
+              // constVideoViewWidth: 1920,
+              // constVideoViewHeight: 1080,
+            }
+          )
           this.player.attachMediaElement(videoElement)
           this.player.load() // 加载
           this.player.play() // 播放
@@ -306,15 +318,116 @@ export default {
       })
     },
     // pc & mobile
-    getDeviceKind () {
+    getDeviceKind() {
       if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
         return 'mobile'
       } else {
         return 'pc'
       }
     },
+    // 修改是否静音
+    setMuted(_muted) {
+      if (this.player.znvVideoPlayerType === 'videoJs') {
+        this.player.muted(_muted)
+      } else if (this.player.znvVideoPlayerType === 'flvjs') {
+        this.player.muted = _muted
+      } else if (this.player.znvVideoPlayerType === 'EZUIPlayer') {
+        this.player.video.muted = _muted
+      }
+    },
+    // 全屏
+    setFullscreen () {
+      if (this.player.znvVideoPlayerType === 'videoJs') {
+        if (this.type === 'hls') {
+          document.getElementById(this.vId + '_html5_api').requestFullscreen()
+        } else {
+          this.player.el().requestFullscreen()
+        }
+      } else if (this.player.znvVideoPlayerType === 'flvjs') {
+        this.player._mediaElement.requestFullscreen()
+      } else if (this.player.znvVideoPlayerType === 'EZUIPlayer') {
+        if (this.player.znvVideoSrcType === 'hls') {
+          this.player.video.requestFullscreen()
+        } else if (this.player.znvVideoSrcType === 'rtmp') {
+          this.player.fullScreen()
+        }
+      }
+    },
+    // 获取当前帧
+    getCurrentFrame() {
+      if (this.type === 'rtmp') {
+        console.log('rtmp暂时无法截图')
+        return false
+      }
+      return this.savePic(+ new Date() + '', this.vId)
+    },
+    savePic(fileName, videoDomId) {
+      let fileType = 'png' // 如果文件名中没有带后缀，默认使用png
+      switch (
+        fileName // 判断保存的图片格式
+      ) {
+        case fileName.indexOf('png') > -1:
+          fileType = 'png'
+          break
+        case fileName.indexOf('jpg') > -1:
+          fileType = 'jpg'
+          break
+
+        case fileName.indexOf('jpeg') > -1:
+          fileType = 'jpeg'
+          break
+        case fileName.indexOf('bmp') > -1:
+          fileType = 'bmp'
+          break
+        case fileName.indexOf('gif') > -1:
+          fileType = 'gif'
+          break
+        default:
+          fileType = 'png'
+          break
+      }
+      let video = document.querySelector('#' + videoDomId) // 找到需要截图的video标签
+      let canvas = (window.canvas = document.createElement('canvas'))
+      canvas.width = video.videoWidth
+      canvas.height = video.videoHeight
+      canvas
+        .getContext('2d')
+        .drawImage(video, 0, 0, canvas.width, canvas.height) // 图片大小和视频分辨率一致
+      let strDataURL = canvas.toDataURL('image/' + fileType) // canvas中video中取一帧图片并转成base64
+      // base64转blob
+      let arr = strDataURL.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n)
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n)
+      }
+      let blob = new Blob([u8arr], {
+        type: mime,
+      })
+      // blob转file
+      let file = new window.File([blob], fileName, {type: mime})
+      return file
+      // 图片下载
+      // let url = window.URL.createObjectURL(blob)
+      // var img = document.createElement('img')
+      // img.src = url
+      // img.className = 'image'
+      // document.querySelector('#myPlayer').after(img)
+      // let a = document.createElement('a')
+      // a.style.display = 'none'
+      // a.href = url
+      // a.download = fileName
+      // document.body.appendChild(a)
+      // a.click()
+      // setTimeout(function () {
+      //   document.body.removeChild(a)
+      //   window.URL.revokeObjectURL(url)
+      // }, 1000)
+    },
     // 销毁HLS视频流以及对应播放器实例
-    destroyHls () {
+    destroyHls() {
       if (this.player && this.playerType === 'videoJs') {
         this.player.dispose()
       } else {
@@ -324,24 +437,26 @@ export default {
       this.player = null
     },
     // 销毁RTSP视频流以及对应播放器实例
-    destroyRtsp () {
+    destroyRtsp() {
       if (this.player) {
         this.player.destroy()
         this.player = null
       }
     },
     // 销毁RTMP视频流以及对应播放器实例
-    destroyRtmp () {
+    destroyRtmp() {
       if (this.player && this.playerType === 'videoJs') {
         this.player.dispose()
       } else {
         this.player.stop()
-        $('#' + this.vId).replaceWith(`<video id="${this.vId}" class="video" autoplay controls muted></video>`)
+        $('#' + this.vId).replaceWith(
+          `<video id="${this.vId}" class="video" autoplay controls muted></video>`
+        )
       }
       this.player = null
     },
     // 销毁flv视频流以及对应播放器实例
-    destroyFlv () {
+    destroyFlv() {
       if (this.player) {
         this.player.pause()
         this.player.unload()
@@ -349,9 +464,9 @@ export default {
         this.player.destroy()
         this.player = null
       }
-    }
+    },
   },
-  destroyed () {
+  destroyed() {
     if (this.type === 'hls') {
       this.destroyHls()
     } else if (this.type === 'rtsp') {
@@ -359,12 +474,12 @@ export default {
     } else if (this.type === 'rtmp') {
       this.destroyRtmp()
     }
-  }
+  },
 }
 </script>
 
 <style scoped lang="scss">
-.znv-video{
+.znv-video {
   position: relative;
   min-width: 100px;
   min-height: 100px;
@@ -372,33 +487,33 @@ export default {
   height: 100%;
   overflow: hidden;
 
-  .hls-container{
+  .hls-container {
     width: 100%;
     height: 100%;
   }
   .small-window-rtmp {
     width: 200%;
     height: 200%;
-    transform: scale(.5);
+    transform: scale(0.5);
     transform-origin: left top;
   }
-  video{
+  video {
     width: 100%;
     height: 100%;
     object-fit: fill;
   }
-  .opt-box{
+  .opt-box {
     position: absolute;
     width: 100%;
     height: 20%;
     left: 0;
     top: 0;
-    background-image: linear-gradient(rgba(0,0,0,.9), rgba(0,0,0,.2));
+    background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.2));
     padding: 0 5%;
   }
 }
 .fade-leave-active {
-  transition: all .8s ease;
+  transition: all 0.8s ease;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   transform: translateY(-100%);
